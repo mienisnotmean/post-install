b600c779e35b4164f56eb633e5982fe9d0561124
mien <mienth2@gmail.com>
2020-11-06 23:12:24 +0100

    update dotfiles

--- 04-dot-files.sh
+++ 04-dot-files.sh
@@ -4,9 +4,9 @@ for f in dot-files/* ; do
     file=$(basename $f)
     if [ ! -f $HOME/.$file ]
     then
-        ln -s $f ~/.$file
-    else
         rm $HOME/.$file
-        ln -s $f ~/.$file
+        ln -s $f $HOME/.$file
+    else
+        ln -s $f $HOME/.$file
     fi
 done
