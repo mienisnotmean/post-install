eba14043cafa68d77d10284d53551be0a3582ff1
mien <mienth2@gmail.com>
2020-11-08 19:34:51 +0100

    add remove packages

--- 02-packages.sh
+++ 02-packages.sh
@@ -17,6 +17,23 @@ func_install() {
     fi
 }
 
+func_uninstall() {
+    if pacman -Qi $1 &> /dev/null; then
+	tput setaf 2
+  	echo "###############################################################"
+  	echo "# The package " $1 " is already installed"
+      	echo "###############################################################"
+      	echo; tput sgr0
+    else
+    	tput setaf 3
+    	echo "###############################################################"
+    	echo "# Installing package " $1
+    	echo "###############################################################"
+    	echo; tput sgr0
+    	sudo pacman -Rsncdd --noconfirm --needed $1
+    fi
+}
+
 package_list=(
     trizen
     base-devel
@@ -32,12 +49,25 @@ package_list=(
     net-tools
 )
 
+remove_package_list=(
+    kvantum-manager
+    gedit
+    epiphany
+    ranger
+)
+
 count=0
 
 for name in "${package_list[@]}" ; do
     count=$[count+1]
     tput setaf 3;echo "Installing package nr. " $count " " $name;tput sgr0;
     func_install $name
+done
+
+for name in "${remove_package_list[@]}" ; do
+    count=$[count+1]
+    tput setaf 3;echo "Remove package nr. " $count " " $name;tput sgr0;
+    func_uninstall $name
 done
 
 sudo -u mien git clone https://github.com/mienisnotmean/.emacs.d.git /home/mien/.emacs.d
